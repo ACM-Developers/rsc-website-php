2d83cd8e7ba03e8a5b365f09df0490bcac46ac3e
rushelmac <rushishelke633@gmail.com>
2021-02-21 13:02:22 +0530

    Changed site name

--- wceauthor/edit.php
+++ wceauthor/edit.php
@@ -187,7 +187,7 @@ $status = $acm->UserStatus();
             </button>
             <!-- End Toggle Nav Link For Mobiles -->
             <a class="navbar-brand" href="index.php">
-              <h1><?php echo $acm->SiteName(); ?></h1>
+              <h1><?php echo <img src="images/iferp.png" alt="IFERP" style="width:100px; padding-right:2%;"> WCE Research Symposium on Computing - RSC 2021 ?></h1>
               <small><?php echo $acm->AdminEmail(); ?></small>
             </a>
           </div>
